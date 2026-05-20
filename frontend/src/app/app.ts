b3c855641a0c5ea5b215c4c0a030b3eace7c1a68
AhmedBaid <baidahmed2005@gmail.com>
2026-05-20 17:23:18 +0100

    adding material ui

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NotificationComponent } from './components/notification/notification';
+import { NavbarComponent } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NotificationComponent],
+  imports: [RouterOutlet, NotificationComponent,NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
