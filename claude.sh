18a3651431a3ff8a229b115b21421202cb0d5784
AhmedBaid <baidahmed2005@gmail.com>
2026-06-18 18:53:39 +0100

    walo

--- claude.sh
+++
@@ -1,4 +0,0 @@
-export ANTHROPIC_BASE_URL="https://api.deepseek.com/anthropic"
-export ANTHROPIC_AUTH_TOKEN="sk-8d0b08a8d4344e99ba6172bcd52df0f9"
-export ANTHROPIC_MODEL="deepseek-v4-pro[1m]"
-exec claude --model deepseek-v4-pro[1m] "$@"
