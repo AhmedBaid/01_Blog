3f456b0ca41f9a430fadcce6446c7b3a60cb6a7a
AhmedBaid <baidahmed2005@gmail.com>
2026-09-24 12:06:59 +0000

    nothing

--- frontend/src/app/components/profile/profile.ts
+++ frontend/src/app/components/profile/profile.ts
@@ -141,7 +141,7 @@ export class ProfileComponent {
 
             return {
               ...cuser,
-              FollowedByCurrentUser: isFollowe,
+              followedByCurrentUser: isFollowe,
             };
           });
 
